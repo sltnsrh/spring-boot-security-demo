da914d5d873ad05a52aee8dbb009bbe5b5659612
sltnsrh <lifenjoy8888@gmail.com>
2022-07-27 21:36:33 +0300

    added websecurity annotation

--- src/main/java/com/example/demospringsecurity/security/SecurityConfig.java
+++ src/main/java/com/example/demospringsecurity/security/SecurityConfig.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
